2edc67328afc6f869d49634f4e64a319f7f8dbda
Вадим Валыгин <vadim12388@mail.ru>
2023-07-07 16:26:58 +0300

    add button

---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button><slot></slot></button>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+button {
+  width: 100px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background-color: #366cb4;
+  color: white;
+  border: 1px solid #366cb4;
+  border-radius: 5px;
+  cursor: pointer;
+  margin: 30px;
+}
+
+button:hover {
+  background-color: #1c385e;
+}
+</style>
